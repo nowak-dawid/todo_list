ecd6e73e2536b9444336c0ea3da914881a0806bc
Dawid Nowak <41728199+nowak-dawid@users.noreply.github.com>
2020-05-03 20:33:04 +0200

    add storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,24 @@
+const path = require("path");
+const webpackConfig = require("@vue/cli-service/webpack.config.js");
 module.exports = {
-  stories: ['../stories/**/*.stories.js'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ["../stories/*.stories.(ts|tsx|js)"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs",
+    "@storybook/preset-typescript",
+  ],
+  webpackFinal: (config) => {
+    return {
+      ...config,
+      module: { ...config.module, rules: webpackConfig.module.rules },
+      resolve: {
+        ...config.resolve,
+        alias: {
+          "@": path.join(__dirname, "../src"),
+          vue$: "vue/dist/vue.esm.js",
+        },
+      },
+    };
+  },
 };
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
+  <div>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+    <MyButton/>
+    </div>
 </template>
+<script>
+  import MyButton from "../../stories/MyButton";
+  export default {
+    components: {MyButton}
+  }
+</script>
