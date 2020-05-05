8dd831f3c44ee8cacb997e8677315a903ae266d5
Dawid Nowak <41728199+nowak-dawid@users.noreply.github.com>
2020-05-05 17:46:56 +0200

    basic functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,51 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="wrapper">
+    <button @click="consoleLogEvent()">Elo</button>
+    <h1>TODO List<button @click="handleClick">Create new</button></h1>
+    <div class="list">
+      <ul>
+        <li v-for="item in list" :key="item[0]">
+          <input type="text" v-model="item['value']"/>
+          <button>+</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      list: [],
+      iterator: 1
+    };
+  },
+  methods: {
+    handleClick() {
+      this.list.push({ id: this.iterator++, value: "" });
+    },
+    consoleLogEvent() {
+      console.log(this.list);
+    }
   }
 };
 </script>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat&display=swap");
+.wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Montserrat", sans-serif;
+}
+  li {
+  list-style-type: none ;
+  }
+  input[type="text"]
+  {
+    border: none;
+    border-bottom: 1px solid;
+  }
+</style>
---
+++ src/views/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="list">
+    <ul></ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List"
+};
+</script>
+
+<style scoped></style>
