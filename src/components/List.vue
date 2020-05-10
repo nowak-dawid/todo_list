2262650907ddd0c0c487c1154ac1063a8d7b9682
Dawid Nowak <41728199+nowak-dawid@users.noreply.github.com>
2020-05-10 14:38:44 +0200

    create and remove elements in list

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,15 +1,56 @@
 <template>
-    <div>
-        <h1>Elo</h1>
-    </div>
+  <div class="wrapper">
+    <h1>Todo list</h1>
+    <button @click="addNew">Add new</button>
+    <ul>
+      <li v-for="(element, index) in list" :key="index">
+        <input
+          v-model="element.value"
+          placeholder="type something"
+          @keyup.enter="checkList()"
+          @blur="checkEmptyElement(index)"
+        />
+          <button @click="removeElement(index)">-</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "List"
+import { Vue, Component, Model } from "vue-property-decorator";
+
+@Component
+export default class List extends Vue {
+  @Model("change", { type: String }) inputValue;
+  list = [];
+  addNew() {
+    this.list.push({
+      value: null
+    });
+  }
+  checkEmptyElement(id) {
+    if (this.list[id]["value"] === null) {
+      this.list.splice(id, 1);
     }
+  }
+  removeElement(id) {
+      this.list.splice(id, 1);
+  }
+}
 </script>
 
 <style scoped>
-
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.wrapper li {
+  list-style-type: none;
+}
+.wrapper li input {
+  border: none;
+  border-bottom: solid 1px;
+}
 </style>
